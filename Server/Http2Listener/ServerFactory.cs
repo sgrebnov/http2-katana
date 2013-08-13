614258ba19aa499ee5646f7c1a41b03c7af47af1
sgrebnov <sergei.grebnov@gmail.com>
2013-08-13 17:02:23 +0400

    experimental structure

---
+++ Server/Http2Listener/ServerFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Owin.Server.Http20Listener
+{
+    using System.Net;
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+    using PropertiesType = IDictionary<string, object>;
+
+    public static class ServerFactory
+    {
+        public static void Initialize(PropertiesType properties)
+        {
+            throw new NotImplementedException("Functionality not provided yet");
+        }
+
+        public static IDisposable Create(AppFunc app, PropertiesType properties)
+        {
+            return new HttpListener();
+        }
+
+    }
+}
---
+++ Tests/Owin.Http2.WebApi.Tests/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Owin.Hosting;
+
+namespace Owin.Test.WebApiTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9000/";
+
+            try
+            {
+                // Start OWIN host 
+                using (WebApp.Start<Startup>(url: baseAddress))
+                {
+                    // Create HttpCient and make a request to api/values 
+                    HttpClient client = new HttpClient();
+
+                    var response = client.GetAsync(baseAddress + "api/values").Result;
+
+                    Console.WriteLine(response);
+                    Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                } 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(String.Format("Error => {0} : {1}", 
+                    ex.Message, 
+                    (ex.InnerException != null) ? ex.InnerException.Message : String.Empty));
+           
+            }
+
+            Console.ReadLine(); 
+        } 
+    }
+}
